981a98d7420f5616c5cd40badf5717dd4f88c9df
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-02-26 09:52:47 +0100

    Declare XOR type

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,2 @@
-type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
-type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
+declare type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+declare type XOR<T, U> = (T | U) extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
