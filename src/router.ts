19d2d3e4c36e7a6ab62235dc7f570546048e8791
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-03-31 17:04:51 +0200

    Add typings for "lib-router"

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import {Request, Response} from "./controller";
+
+export type RouterRequest = Request & { pathParams: Record<string, string | undefined> }
+
+export interface Router {
+  get(pattern: string, handler: (req: RouterRequest) => Response): void;
+  post(pattern: string, handler: (req: RouterRequest) => Response): void;
+  delete(pattern: string, handler: (req: RouterRequest) => Response): void;
+  put(pattern: string, handler: (req: RouterRequest) => Response): void;
+  head(pattern: string, handler: (req: RouterRequest) => Response): void;
+  all(pattern: string, handler: (req: RouterRequest) => Response): void;
+  route(method: string, pattern: string, handler: (req: RouterRequest) => Response): void;
+  filter(filter: (req: Request, next: (req: Request) => Response) => Response): void;
+  dispatch(req: Request): Response;
+}
