7d0e743ce816df725218fe7e30435c6be89fd6ae
Tom Arild Jakobsen <tajakobsen@gmail.com>
2020-09-11 08:39:11 +0200

    Add websocket library support

---
+++ src/websocket.ts
@@ -0,0 +1,21 @@
+export interface WebsocketLibrary {
+  /**
+   * Add an id to a socket group.
+   */
+  addToGroup(group: string, id: string): void;
+
+  /**
+   * Remove an id from a socket group.
+   */
+  removeFromGroup(group: string, id: string): void;
+
+  /**
+   * Send message directly to a socket id.
+   */
+  send(id: string, message: string): void;
+
+  /**
+   * Send message to all sockets in group.
+   */
+  sendToGroup(group: string, message: string): void;
+}
