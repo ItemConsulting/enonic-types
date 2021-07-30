37dc974ac8daebd81054a80b8bbd846e3a622729
Tom Arild Jakobsen <tajakobsen@gmail.com>
2021-07-30 16:04:48 +0200

    Update RouterLib to support Array as pattern

--- src/router.ts
+++ src/router.ts
@@ -4,14 +4,54 @@ export type RouterLib = () => Router;
 
 export type RouterRequest = Request & { pathParams: Record<string, string | undefined> };
 
+/**
+ * Path pattern to match.
+ */
+export type Pattern = string | Array<string>;
+
 export interface Router {
-  get(pattern: string, handler: (req: RouterRequest) => Response): void;
-  post(pattern: string, handler: (req: RouterRequest) => Response): void;
-  delete(pattern: string, handler: (req: RouterRequest) => Response): void;
-  put(pattern: string, handler: (req: RouterRequest) => Response): void;
-  head(pattern: string, handler: (req: RouterRequest) => Response): void;
-  all(pattern: string, handler: (req: RouterRequest) => Response): void;
-  route(method: string, pattern: string, handler: (req: RouterRequest) => Response): void;
+  /**
+   * Adds a route that matches the GET method.
+   */
+  get(pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a route that matches the POST method.
+   */
+  post(pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a route that matches the DELETE method.
+   */
+  delete(pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a route that matches the PUT method.
+   */
+  put(pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a route that matches the HEAD method.
+   */
+  head(pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a route that matches all methods.
+   */
+  all(pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a route to this router.
+   */
+  route(method: string, pattern: Pattern, handler: (req: RouterRequest) => Response): void;
+
+  /**
+   * Adds a filter to this router.
+   */
   filter(filter: (req: Request, next: (req: Request) => Response) => Response): void;
+
+  /**
+   * Dispatch the request to this router.
+   */
   dispatch(req: Request): Response;
 }
