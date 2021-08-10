7e3a08a687c320ff1ef5555d681ebf9b4862a7c8
Tom Arild Jakobsen <tajakobsen@gmail.com>
2021-08-10 15:55:24 +0200

    Add helper types to interact with OptionSet

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,11 @@
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
+export type Unarray<T> = T extends Array<infer U> ? U : T;
+export type PickSelected<OPTION_SET extends { _selected: string }, SELECTED extends string> = Extract<
+  OPTION_SET,
+  { _selected: SELECTED }
+>;
+export type PickSelectedValue<
+  OPTION_SET extends { _selected: string },
+  SELECTED extends string
+> = SELECTED extends keyof PickSelected<OPTION_SET, SELECTED> ? PickSelected<OPTION_SET, SELECTED>[SELECTED] : never;
