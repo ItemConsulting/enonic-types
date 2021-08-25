d3cf8eb43e9ba7007559c190ad64c8b06560cfcb
Tom Arild Jakobsen <tajakobsen@gmail.com>
2021-08-25 13:11:38 +0200

    Let Unarray type handle ReadonlyArray too

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type XOR<T, U> = T | U extends object ? (Without<T, U> & U) | (Without<U, T> & T) : T | U;
-export type Unarray<T> = T extends Array<infer U> ? U : T;
+export type Unarray<T> = T extends Array<infer U> ? U : T extends ReadonlyArray<infer U> ? U : T;
 export type PickSelected<OPTION_SET extends { _selected: string }, SELECTED extends string> = Extract<
   OPTION_SET,
   { _selected: SELECTED }
