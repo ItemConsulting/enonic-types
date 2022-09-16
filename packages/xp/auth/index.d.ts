9654e36f4b8f664d1ce48a45054abee09056b07c
Tom Arild Jakobsen <tajakobsen@gmail.com>
2022-09-16 14:40:26 +0200

    Clean auth-lib a bit

--- packages/xp/auth/index.d.ts
+++ packages/xp/auth/index.d.ts
@@ -5,29 +5,12 @@ import type {
   EditorFn,
   CreateRoleParams,
 } from "@enonic-types/lib-auth";
-export type {
-  CreateGroupParams,
-  CreateUserParams,
-  LoginParams,
-  EditorFn,
-  CreateRoleParams,
-} from "@enonic-types/lib-auth";
-
-type PrincipalKeySystem =
-  | "role:system.everyone"
-  | "role:system.authenticated"
-  | "role:system.admin"
-  | "role:system.admin.login"
-  | "role:system.auditlog"
-  | "role:system.user.admin"
-  | "role:system.user.app"
-  | "user:system:su";
 
 export type PrincipalKeyUser = `user:${string}:${string}`;
 export type PrincipalKeyGroup = `group:${string}:${string}`;
 export type PrincipalKeyRole = `role:${string}`;
 
-export type PrincipalKey = PrincipalKeySystem | PrincipalKeyUser | PrincipalKeyGroup | PrincipalKeyRole;
+export type PrincipalKey = PrincipalKeyUser | PrincipalKeyGroup | PrincipalKeyRole;
 
 export type Principal = User | Role | Group;
 
