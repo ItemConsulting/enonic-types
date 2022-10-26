391f0cdb331588a3786fa2105688d3f8b79e4e55
Tom Arild Jakobsen <tajakobsen@gmail.com>
2022-10-26 12:40:25 +0200

    Fix ThymeleafRenderOptions

--- packages/freemarker/index.d.ts
+++ packages/freemarker/index.d.ts
@@ -1,11 +1,3 @@
 import type { ResourceKey } from "@item-enonic-types/core";
 
-export function render<Model extends object>(
-  view: ResourceKey,
-  model?: Model,
-  options?: ThymeleafRenderOptions
-): string;
-
-export interface ThymeleafRenderOptions {
-  mode: "HTML" | "XML" | "TEXT" | "JAVASCRIPT" | "CSS" | "RAW";
-}
+export function render<Model extends object>(view: ResourceKey, model?: Model): string;
--- packages/thymeleaf/index.d.ts
+++ packages/thymeleaf/index.d.ts
@@ -1,3 +1,11 @@
 import type { ResourceKey } from "@item-enonic-types/core";
 
-export function render<Model extends object>(view: ResourceKey, model?: Model): string;
+export interface ThymeleafRenderOptions {
+  mode: "HTML" | "XML" | "TEXT" | "JAVASCRIPT" | "CSS" | "RAW";
+}
+
+export function render<Model extends object>(
+  view: ResourceKey,
+  model?: Model,
+  options?: ThymeleafRenderOptions
+): string;
