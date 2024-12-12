aadc94d86c7d5bf62c7c069bc830b953fde2e51b
Tom Arild Jakobsen <tajakobsen@gmail.com>
2024-12-12 09:16:51 +0100

    Remove Guillotine and module declarations
    Guillotine has official support, and does not need these types anymore

--- libs.d.ts
+++ libs.d.ts
@@ -21,7 +21,6 @@ export interface EnonicLibraryMap {
   "/lib/graphql": typeof import("/lib/graphql");
   "/lib/graphql-playground": typeof import("/lib/graphql-playground");
   "/lib/xp/grid": typeof import("/lib/xp/grid");
-  "/lib/guillotine": typeof import("/lib/guillotine");
   "/lib/http-client": typeof import("/lib/http-client");
   "/lib/xp/i18n": typeof import("/lib/xp/i18n");
   "/lib/xp/io": typeof import("/lib/xp/io");
